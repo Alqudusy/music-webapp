f842ce61f77c41c09fa8efba4be8016e306da23b
alqudusy <yusufabdulqudus02@gmail.com>
2024-10-13 13:36:04 -0700

    Designing the user interfaces

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
-const fileURL = "musics/Larry_Gaaga_ft_Joeboy_-_Ready_Loadedsongs.com.ng.mp3";
+const fileURL = [
+     "musics/Larry_Gaaga_ft_Joeboy_-_Ready_Loadedsongs.com.ng.mp3",
+     "musics/Oxlade_ft_Wande_Coal_-_ASUNASA_HOLD_YOUR_WAIST__Loadedsongs.com.ng.mp3",
+     "musics/Bella_Shmurda_ft_BOJ_Krizbeatz_-_Mentali_Loadedsongs.com.ng.mp3",
+     "musics/Boy_Spyce_-_Shout_Loadedsongs.com.ng.mp3",
+     "musics/CKay_-_In_My_Bed_Loadedsongs.com.ng.mp3",
+     "musics/Kcee_-_Netfliss_Loadedsongs.com.ng.mp3",
+     "musics/Lordeyang_ft_Damo_K_-_Forgive_Me_Loadedsongs.com.ng.mp3",
+     "musics/Mr_Say_-_Ewa_Bamijo_(www.NETNAIJA.com).mp3",
+     "musics/Phyno_ft_Johnny_Drille_-_Sweet_Karma_Loadedsongs.com.ng.mp3",
+     "musics/Shallipopi_-_Free_Service_Loadedsongs.com.ng.mp3"
+    ];
 fetch(fileURL)
         .then(response => {
             if (!response.ok) {
